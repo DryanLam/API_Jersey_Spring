4fb6a6046b40dbcfb2421d4ac4a6dd47da461f0d
Dryan Lam <dunglam@kms-technology.com>
2020-09-15 10:58:50 +0700

    Change package register to scan by Reflections

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 http://localhost:7000/api/books
+
+$ maven spring-boot:run
+$ maven clean packages
+$ java -jar <name>.jar
+>> java -jar Spring-Rest-Jersey-1.0.0-SNAPSHOT.jar
+
+
+
+Issue:
+1. Can NOT register by package due to Jersey issue. 
+    Fixed: Use Reflections to create a scan method register each class
+
+2. Reflections can NOT scan sub-class.
+    The major difference between version 0.9.11 and 0.9.12 is that in 0.9.12 Guava dependency was removed in favor of Java 8 Streams API.
+    Fixed: Add true to getTypesAnnotatedWith(Path.class, true)
--- src/main/java/com/sample/dl/context/AppConfiguration.java
+++ src/main/java/com/sample/dl/context/AppConfiguration.java
@@ -3,9 +3,11 @@ package com.sample.dl.context;
 import com.sample.dl.controller.BookController;
 import com.sample.dl.service.ExceptionService;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.reflections.Reflections;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import javax.ws.rs.Path;
 
 
 @Configuration
@@ -13,12 +15,25 @@ public class AppConfiguration extends ResourceConfig {
 	public AppConfiguration() {
 //		packages("com.sample.dl.controller");
 //		register(ExceptionService.class);
+		scan("com.sample.dl.controller");
 	}
 
 	// Also lay on both methods
 	@PostConstruct
 	public void setUp() {
-		packages("com.sample.dl.controller");
+//		packages("com.sample.dl.controller");
 		register(ExceptionService.class);
+	}
+
+	public void scan(String... packages) {
+		for (String pack : packages) {
+			Reflections reflections = new Reflections(pack);
+			reflections.getTypesAnnotatedWith(Path.class, true)
+					   .parallelStream()
+					   .forEach((clazz) -> {
+//						   logger.info("New resource registered: " + clazz.getName());
+						   register(clazz);
+					   });
+		}
 	}
 }
