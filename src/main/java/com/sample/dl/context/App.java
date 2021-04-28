a337f9e6679558e90c233602e4e3a70f1d16e508
dlam <dryanlam@gmai.com>
2021-04-28 12:06:58 +0700

    Exclude auto configure localhost of mongodb in Sprint Boot

--- src/main/java/com/sample/dl/context/App.java
+++ src/main/java/com/sample/dl/context/App.java
@@ -2,9 +2,10 @@ package com.sample.dl.context;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={MongoAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.sample.dl"})
 public class App {
 	public static void main(String[] args) {
