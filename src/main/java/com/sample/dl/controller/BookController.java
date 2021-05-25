ce4a683e07a84552538fd164099fdc5518e5cb82
dlam <dryanlam@gmai.com>
2021-05-25 12:51:33 +0700

    Test 05

--- src/main/java/com/sample/dl/controller/BookController.java
+++ src/main/java/com/sample/dl/controller/BookController.java
@@ -37,6 +37,7 @@ public class BookController {
     @Produces("application/json")
     @Path("/{oid}")
     public Response getBookId(@PathParam("oid") String oid) {
+        // For Test
         Map output = new HashMap();
         output.put("id", oid);
         output.put("name", "White Hat Hacking Ethetic");
