2330505087f2a751be4d322981407ea184951900
dlam <dryanlam@gmai.com>
2021-05-25 12:28:44 +0700

    Test 01

--- src/main/java/com/sample/dl/controller/BookController.java
+++ src/main/java/com/sample/dl/controller/BookController.java
@@ -50,6 +50,7 @@ public class BookController {
     @DELETE
     @Path("/{oid}")
     public Response deleteBook(@PathParam("oid") String oid) {
+        
         bookService.deleteBook(oid);
         return Response.ok().build();
     }
